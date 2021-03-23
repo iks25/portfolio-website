e2cb01eb8dd2cebf96185f13c85a33ee7d8bc44f
IgorKozerski <ikspun@gmail.com>
2021-03-23 01:16:38 +0000

    adding animation for textbox

---
+++ src/components/aboutMe/FancyLetter/BisnessCard/BisnessCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BisnessCard = (props) => {
+  return (
+    <div>
+      <div>kolo</div>
+      <div>Igor Kozerski</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default BisnessCard;
--- src/components/aboutMe/TextBox.js
+++ src/components/aboutMe/TextBox.js
@@ -5,9 +5,7 @@ import { useSpring, animated } from "react-spring";
 const TextBox = () => {
   const props = useSpring({
     to: [{ marginTop: 0, marginLeft: 0 }, { transform: "scale(1)" }],
-
     from: { marginTop: 3000, marginLeft: 3000, transform: "scale(0.3)" },
-
     config: { mass: 2, tension: 200, friction: 30 },
   });
   return (
