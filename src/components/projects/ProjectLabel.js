36a9eb6d05dbbb72b1f82c94ee6ab80081137759
IgorKozerski <ikspun@gmail.com>
2021-07-09 16:12:55 +0100

    creating ProjectLabel

---
+++ src/components/projects/ProjectLabel.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+import "./projectLabel.css"
+const ProjectLabel = (props) => {
+    return (<div className="projectLabel">
+        <div className="projectName">project name</div>
+        <div className="projectLinks">
+            <div>
+                <div className="projectLabelLink">fds</div>
+            </div>
+            <div>
+                <div className="projectLabelLink">fdf</div>
+            </div>
+        </div>
+    </div>);
+}
+
+export default ProjectLabel;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,18 @@
 import React from "react";
+import ProjectLabel from "./ProjectLabel";
+import "./project.css"
 
 const Projects = (props) => {
-  return <div>Projects</div>;
+  return <div>Projects<br/>
+    test
+
+    <div>
+
+    </div>
+    <div >
+      <ProjectLabel/>
+    </div>
+  </div>;
 };
 
 export default Projects;
