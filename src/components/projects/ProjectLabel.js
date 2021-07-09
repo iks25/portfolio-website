1ea49abead26bbad6198c152d17c611271ced31a
IgorKozerski <ikspun@gmail.com>
2021-07-09 20:05:26 +0100

    adding corner to labelprojects

--- src/components/projects/ProjectLabel.js
+++ src/components/projects/ProjectLabel.js
@@ -1,17 +1,25 @@
-import React  from 'react';
-import "./projectLabel.css"
+import React from "react";
+import "./projectLabel.css";
 const ProjectLabel = (props) => {
-    return (<div className="projectLabel">
-        <div className="projectName">project name</div>
-        <div className="projectLinks">
-            <div>
-                <div className="projectLabelLink">fds</div>
-            </div>
-            <div>
-                <div className="projectLabelLink">fdf</div>
-            </div>
+  return (
+    <div className="projectLabel">
+      <div className="projectName">project name</div>
+      <div className="projectLinks">
+        <div>
+          <div className="projectLabelLink">project</div>
         </div>
-    </div>);
-}
+        <div>
+          <div className="projectLabelLink">source code</div>
+        </div>
+      </div>
+      <div className="triangleCorner">
+        <div className="cover">
+          <div className="arrow-left"></div>
+        </div>
+      </div>
+      <div className="cornerLanguage">js</div>
+    </div>
+  );
+};
 
 export default ProjectLabel;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ProjectLabel from "./ProjectLabel";
-import "./project.css"
+import "./project.css";
 
 const Projects = (props) => {
-  return <div>Projects<br/>
-    test
-
+  return (
     <div>
-
+      Projects
+      <br />
+      test ds
+      <div></div>
+      <div>
+        <ProjectLabel />
+      </div>
     </div>
-    <div >
-      <ProjectLabel/>
-    </div>
-  </div>;
+  );
 };
 
 export default Projects;
